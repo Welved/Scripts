670b3c19cf3d34d5e04e21b68e38ed8e28426e1e
226cenk <61040834+226cenk@users.noreply.github.com>
2022-11-10 15:41:00 -0600

    Add files via upload

---
+++ Ubuntu22.xx_Lazy_Fresh_Install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "This script will auto configure and download/install apps that a FOB from Windows will need."
+
+pactl load-module module-null-sink sink_name=virtmic sink_properties=device.description=Virtual_Microphone_Sink
+pactl load-module module-null-sink sink_name=virtspk sink_properties=device.description=Virtual_Speaker
+sudo apt update && sudo apt upgrade && sudo apt autoremove
+sudo apt remove snapd && sudo apt purge snapd && sudo apt install snapd
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize' && gsettings set org.gnome.desktop.peripherals.touchpad "disable-while-typing" false
+sudo apt install curl && curl -sL https://raw.githubusercontent.com/wimpysworld/deb-get/main/deb-get | sudo -E bash -s install deb-get
+sudo apt install pavucontrol
+sudo apt install gnome-calendar
+sudo apt install kdenlive
+sudo apt install vlc
+sudo apt install thunderbird
+sudo apt install gimp
+sudo snap install ms-office-electron
+sudo snap install yuzu
+sudo deb-get install zoom
+sudo deb-get install portmaster
+sudo deb-get install firefox-esr
+sudo deb-get install dropbox
+sudo deb-get install obs-studio
+wget -P ~/Downloads https://github.com/xournalpp/xournalpp/releases/download/v1.1.1/xournalpp-1.1.1-Ubuntu-focal-x86_64.deb
+wget -P ~/Documents https://mega.nz/linux/repo/xUbuntu_22.04/amd64/megasync-xUbuntu_22.04_amd64.deb
+wget -P ~/Documents https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+wget -P ~/Documents https://cdn.akamai.steamstatic.com/client/installer/steam.deb
+wget -P ~/Documents https://github.com/xournalpp/xournalpp/releases/download/v1.1.1/xournalpp-1.1.1-Ubuntu-focal-x86_64.deb
+chmod +x ~/Documents/*.deb
+sleep 1
+sudo apt install apt-transport-https curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+echo "sudo apt install ./Downloads/*.deb"
+sudo apt update && sudo apt upgrade && sudo apt install brave-browser
+echo --------- Machine will reboot in 10 seconds. ------------------------
+wait 10
+reboot
