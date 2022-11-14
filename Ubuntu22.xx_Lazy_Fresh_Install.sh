4ec909dc22f4616578cf8d8261e37e982e4c4f26
226cenk <61040834+226cenk@users.noreply.github.com>
2022-11-14 10:04:41 -0600

    Update Ubuntu22.xx_Lazy_Fresh_Install.sh

--- Ubuntu22.xx_Lazy_Fresh_Install.sh
+++ Ubuntu22.xx_Lazy_Fresh_Install.sh
@@ -31,15 +31,15 @@ wget -P ~/Documents https://github.com/xournalpp/xournalpp/releases/download/v1.
 wget -P ~/Documents https://github.com/keepassxreboot/keepassxc/releases/download/2.7.4/KeePassXC-2.7.4-x86_64.AppImage
 wget -P ~/Documents https://builds.parsecgaming.com/package/parsec-linux.deb
 wget -P ~/Documents https://download.oracle.com/java/19/latest/jdk-19_linux-x64_bin.deb
-wget -P ~/Documents https://customerconnect.vmware.com/akam/13/2b1ced92
+wget -P ~/Documents https://download3.vmware.com/software/WKST-PLAYER-1624/VMware-Player-Full-16.2.4-20089737.x86_64.bundle
 chmod +x ~/Documents/*.bundle
 chmod +x ~/Documents/*.deb
 sleep 1
 sudo apt install apt-transport-https curl
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-echo "sudo apt install ./Documents/*.deb"
-echo "sudo ./Documents/*.bundle"
+sudo apt install ./Documents/*.deb"
+sudo ./Documents/*.bundle"
 sudo apt update && sudo apt upgrade && sudo apt install brave-browser
 echo --------- Machine will reboot in 10 seconds. ------------------------
 wait 10
