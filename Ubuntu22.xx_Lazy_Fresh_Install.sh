44cc5e7117ed45772362180f1257e56d20cfff3b
Welved <125337873+Welved@users.noreply.github.com>
2023-07-20 21:48:18 -0500

    Update Ubuntu22.xx_Lazy_Fresh_Install.sh

--- Ubuntu22.xx_Lazy_Fresh_Install.sh
+++ Ubuntu22.xx_Lazy_Fresh_Install.sh
@@ -8,7 +8,6 @@ echo "This script will auto configure and download/install apps that a FOB from 
 sudo apt update && sudo apt upgrade && sudo apt autoremove
 sudo apt remove snapd && sudo apt purge snapd && sudo apt install snapd
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize' && gsettings set org.gnome.desktop.peripherals.touchpad "disable-while-typing" false
-sudo apt install curl && curl -sL https://raw.githubusercontent.com/wimpysworld/deb-get/main/deb-get | sudo -E bash -s install deb-get
 sudo apt install gnome-calendar kdenlive vlc thunderbird gimp keepassxc build-essential
 sudo snap install dolphin-emulator yuzu
 #wget -P ~/Documents https://mega.nz/linux/repo/xUbuntu_22.04/amd64/megasync-xUbuntu_22.04_amd64.deb
